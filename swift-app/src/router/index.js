90e6029f5df388e8f14b2ca88a45e2d8a4ab729b
Giovanni <u18009035@tuks.co.za>
2020-06-13 16:49:18 +0200

    Integrate basic AR.js

--- swift-app/src/router/index.js
+++ swift-app/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import OrdersView from '../views/orders/OrdersView.vue'
 import CartView from '../views/orders/CartView.vue'
 import FavouritesView from '../views/favourites/FavouritesView.vue'
+import ARView from '../views/ar/ARView.vue'
 
 import CheckInView from '../views/home/CheckInView.vue'
 import HomeView from '../views/home/HomeView.vue'
@@ -16,6 +17,9 @@ import MenuItemView from '../views/menu/MenuItemView.vue'
 
 Vue.use(VueRouter)
 
+Vue.use(require('vue-script2'))
+
+
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/menu', name: 'menu', component: MenuHomeView },
@@ -25,6 +29,7 @@ const routes = [
   { path: '/orders', name: 'orders', component: OrdersView },
   { path: '/cart', name: 'cart', component: CartView },
   { path: '/favourites', name: 'favourites', component: FavouritesView },
+  { path: '/ar', name: 'ar', component: ARView },
   { path: '/profile', name: 'profile', component: ProfileView },
   { path: '/menuItem/:itemid', name: 'menuItem', component: MenuItemView },
 ]
@@ -32,5 +37,17 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+
+Vue.config.ignoredElements = [
+  'a-scene',
+  'a-entity',
+  'a-camera',
+  'a-box',
+  'a-sky',
+  'a-assets',
+  'a-marker',
+  'a-marker-camera',
+  'a-obj-model'
+]
 
 export default router
---
+++ swift-app/src/views/ar/ARView.vue
@@ -0,0 +1,28 @@
+<template>
+ <a-scene embedded arjs>
+    <!-- TODO: Fix Z-Index issue -->
+    <a-obj-model  v-bind:src="model.obj" v-bind:mtl="model.mtl" position="0 0.5 0" scale="5 5 5" />
+    <!-- define a camera which will move according to the marker position -->
+   <a-marker-camera preset='custom' type='pattern'  v-bind:url="markerPattern"></a-marker-camera>
+  </a-scene>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      //TODO: Move to swift.ml server
+      markerPattern: "https://sitecheck.co.za/ar/Swift.patt",
+      model:
+        {
+          obj: "https://sitecheck.co.za/ar/model/AppleStrudel.obj",
+          mtl: "https://sitecheck.co.za/ar/model/AppleStrudel.mtl"
+        }
+    }
+  },
+  methods: {
+   
+  },
+  
+}
+</script>
