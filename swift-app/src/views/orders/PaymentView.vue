34c229b9e81e89ce781f75f97bb85c743a218353
Giovanni <u18009035@tuks.co.za>
2020-06-13 21:47:30 +0200

    Integrate Cart Total with Payfast

--- swift-app/src/views/orders/PaymentView.vue
+++ swift-app/src/views/orders/PaymentView.vue
@@ -3,7 +3,7 @@
     <div id="iframe-wrapper">
       <iframe 
         id="payfastIframe"
-        :src="payfast.url"
+        :src="payfast.urlPart1+this.orderTotal+payfast.urlPart2"
         style="width:100%;height:100%;min-height:1200px"
         type="application/pdf"
         frameborder="0"
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import { Store, mapGetters } from "vuex";
 import NavBar from '@/components/layout/NavBar';
 
 export default {
@@ -22,8 +23,10 @@ export default {
     return {
         loadCount: 0,
         payfast:{
-            //todo store and retrieve item_name and amount
-            url: "http://sandbox.payfast.co.za/eng/process?cmd=_paynow&receiver=10009935&item_name=FoodOrder&amount=5000.00&return_url=https%3A%2F%2Fswiftapp.ml%2F&cancel_url=https%3A%2F%2Fswiftapp.ml%2F"
+            //url gets concatinated with payment amount
+            //should possibly move to computed section
+            urlPart1: "http://sandbox.payfast.co.za/eng/process?cmd=_paynow&receiver=10009935&item_name=Swift%20Order&amount=",
+            urlPart2: "&return_url=https%3A%2F%2Fswiftapp.ml%2F&cancel_url=https%3A%2F%2Fswiftapp.ml%2F"
         }
     }
   },
@@ -42,5 +45,10 @@ export default {
       this.$router.push('/')
     },
   },
+  computed: {
+    ...mapGetters({
+      orderTotal: "OrderStore/getOrderTotal"
+    })
+  }
 }
 </script>
