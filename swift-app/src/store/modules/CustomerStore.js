968c8cac69ecbfea7af77e04336c623b22cd15e7
Cadon <u17102678@tuks.co.za>
2020-06-14 02:05:34 +0200

    Validation and Register Customer functionality

--- swift-app/src/store/modules/CustomerStore.js
+++ swift-app/src/store/modules/CustomerStore.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 // State object
 const initialState = () => ({
   customer: {},
+  isAuthenticated: false,
 });
 
 const state = initialState();
@@ -12,6 +13,9 @@ const getters = {
   getCustomerProfile( state ) {
     return state.customer;
   },
+  isAuthenticated(state) {
+    return state.isAuthenticated;
+  }
 }
 
 // Actions 
@@ -20,18 +24,38 @@ const actions = {
     axios.post('https://api.swiftapp.ml', 
       {
         "requestType": "login",
-        // "email": data.email,
-        "email": "johnmay@gmail.com",
-        // "password": data.password
-        "password": "john123"
+        "email": data.email,
+        // "email": "johnmay@gmail.com",
+        "password": data.password
+        // "password": "john123"
       }
     ).then(result => {
       commit('SAVE_CUSTOMER', result.data);
-      console.log(result.data.username)
-    })
-    .catch(e => {
-      throw new Error(`API ${e}`);
-    })
+      commit('SET_AUTHENTICATION', true);
+    }).catch(({ response }) => {
+      if(response.status == 400 || response.status == 403 || response.status == 404){
+        commit('SET_AUTHENTICATION', false);
+      } 
+    });
+  },
+
+  register({commit}, data) {
+    axios.post('https://api.swiftapp.ml', 
+      {
+        "requestType": "register",
+        "name": "john",
+        "surname": "doe",
+        "username": data.username,
+        // "username": "john123",
+        "email": data.email,
+        // "email": "johnmay@gmail.com",
+        "password": data.password
+        // "password": "john123"
+      }
+    ).then(result => {
+      commit('SAVE_CUSTOMER', result.data);
+    }).catch(({ response }) => {
+    });
   },
 
   reset({ commit }) {
@@ -43,6 +67,10 @@ const actions = {
 const mutations = {
   SAVE_CUSTOMER(state, customer) {
     state.customer = customer;
+  },
+
+  SET_AUTHENTICATION(state, authentication_state) {
+    state.isAuthenticated = authentication_state;
   },
 
   RESET(state) {
--- swift-app/src/views/usermanagement/RegisterView.vue
+++ swift-app/src/views/usermanagement/RegisterView.vue
@@ -6,39 +6,19 @@
     </div>
     <div class="row d-flex flex-column align-center">
       <v-col cols="10" >
-        <v-text-field
-          :rules="[rules.required]"
-          name="username"
-          label="Username"
-          color="secondary"
-        ></v-text-field>
+        <v-text-field v-model="username" :error-messages="usernameErrors" label="Username" required @blur="$v.username.$touch()"></v-text-field>
       </v-col>
       <v-col cols="10" >
-          <v-text-field
-            :rules="[rules.required, rules.emailMatch]"
-            name="email"
-            label="Email"
-            value=""
-            @click:append="showPlaceholder = !showPlaceholder"
-          ></v-text-field>
-        </v-col>
+        <v-text-field v-model="email" :error-messages="emailErrors" label="Email" required @blur="$v.email.$touch()"></v-text-field>
+      </v-col>
       <v-col cols="10" >
-        <v-text-field
-          v-model="password"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required, rules.min]"
-          :type="showPassword ? 'text' : 'password'"
-          name="password"
-          label="Password"
-          hint="At least 8 characters"
-          counter
-          @click:append="showPassword = !showPassword"
-          color="secondary"
-        ></v-text-field>
+        <v-text-field v-model="password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" :error-messages="passwordErrors" :type="showPassword ? 'text' : 'password'" label="Password" required @blur="$v.password.$touch()" @click:append="showPassword = !showPassword"></v-text-field>
+        <p class="body-2 float-right" color="secondary"><u>Forgot Password?</u></p>
       </v-col>
     </div>
     <div class="row d-flex flex-column align-center mx-8">
-      <v-btn @click="goToHome" block rounded class="py-5" color="primary">Register</v-btn>
+      <v-btn @click="registerCustomer"  v-show=!isLoading block rounded class="py-5" color="primary">Register</v-btn>
+      <v-progress-circular v-show=isLoading indeterminate color="primary"></v-progress-circular>
     </div>
     <div class="row d-flex flex-column align-center ">
       <p>Not your first time here? <a @click="goToLogin"><u>Login</u></a>.</p>
@@ -48,12 +28,26 @@
 </template>
 
 <script>
+import store from '@/store/store.js';
+import { mapActions, mapGetters } from 'vuex'
+import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength, email } from 'vuelidate/lib/validators'
+
 export default {
+  mixins: [validationMixin],
+  validations: {
+    username: { required, maxLength: maxLength(16) },
+    email: { required, email },
+    password: { required, minLength: minLength(8) },
+  },
   data () {
     return {
       showPassword: false,
       showPlaceholder: false,
+      username: '',
+      email: '',
       password: '',
+      isLoading: false,
       rules: {
         required: value => !!value || '*Required.',
         min: v => v.length >= 8 || '*Min 8 characters',
@@ -65,8 +59,49 @@ export default {
     goToLogin () {
       this.$router.push('login')
     },
-    goToHome () {
-      this.$router.push('/')
+    registerCustomer () {
+      this.isLoading = true
+      if (this.usernameErrors.length > 0 && this.emailErrors > 0 && this.passwordErrors > 0|| this.username.length == 0 || this.email.length == 0 || this.password.length == 0) {
+        this.$v.$touch()
+        this.isLoading = false
+      } else {
+        this.isLoading = false
+        let data = {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        }
+        
+        this.register(data)
+        this.$router.push('/')
+      }
+    },
+    ...mapActions({
+      register: 'CustomerStore/register',
+    }),
+  },
+  computed: {
+    usernameErrors () {
+      const errors = []
+      if (!this.$v.username.$dirty) return errors
+      !this.$v.username.maxLength && errors.push('Username can be at most 16 characters long')
+      !this.$v.username.required && errors.push('Username is required.')
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be a valid email')
+      !this.$v.email.required && errors.push('Email is required.')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.minLength && errors.push('Password must be at least 8 characters long')
+      !this.$v.password.required && errors.push('Password is required.')
+      
+      return errors
     },
   },
 }
