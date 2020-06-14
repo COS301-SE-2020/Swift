f08d3f1779740e14be1f3bad02a7753286eaeeed
Cadon <u17102678@tuks.co.za>
2020-06-14 02:05:34 +0200

    Validation and Register Customer functionality

--- swift-app/src/views/usermanagement/LoginView.vue
+++ swift-app/src/views/usermanagement/LoginView.vue
@@ -6,28 +6,16 @@
     </div>
     <div class="row d-flex flex-column align-center">
       <v-col cols="10" >
-        <v-text-field
-          v-model="email"
-          :rules="[rules.required]"
-          label="Email"
-          color="secondary"
-        ></v-text-field>
+        <v-text-field v-model="email" :error-messages="emailErrors" label="Email" required @blur="$v.email.$touch()"></v-text-field>
       </v-col>
-      <v-col cols="10">
-        <v-text-field
-          v-model="password"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required]"
-          :type="showPassword ? 'text' : 'password'"
-          label="Password"
-          @click:append="showPassword = !showPassword"
-          color="secondary"
-        ></v-text-field>
+      <v-col cols="10" >
+        <v-text-field v-model="password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" :error-messages="passwordErrors" :type="showPassword ? 'text' : 'password'" label="Password" required @blur="$v.password.$touch()" @click:append="showPassword = !showPassword"></v-text-field>
         <p class="body-2 float-right" color="secondary"><u>Forgot Password?</u></p>
       </v-col>
     </div>
     <div class="row d-flex flex-column align-center mx-8">
-      <v-btn @click="loginCustomer" block rounded class="py-5" color="primary">Sign in</v-btn>
+      <v-btn @click="loginCustomer" v-show=!isLoading block rounded class="py-5" color="primary">Sign in</v-btn>
+      <v-progress-circular v-show=isLoading indeterminate color="primary"></v-progress-circular>
     </div>
     <div class="row d-flex flex-col align-center justify-center">
       <v-col cols="2">
@@ -55,37 +43,75 @@
 <script>
 import store from '@/store/store.js';
 import { mapActions, mapGetters } from 'vuex'
+import { validationMixin } from 'vuelidate'
+import { required, maxLength, email } from 'vuelidate/lib/validators'
 
 export default {
+  mixins: [validationMixin],
+  validations: {
+    email: { required, email },
+    password: { required },
+  },
   data () {
     return {
       showPassword: false,
       showPlaceholder: false,
       password: '',
       email: '',
-      rules: {
-        required: value => !!value || '*Required.',
-        incorrectPassword: () => ('*Incorrect Password'),
-      },
+      isLoading: false,
+      errorMsg: '',
     }
   },
   methods: {
     goToRegister () {
       this.$router.push('/register')
     },
-    loginCustomer () {
-      let data = {
-        email: this.email,
-        password: this.password,
+    loginCustomer ()  {
+      this.isLoading = true
+      if (this.emailErrors.length > 0 && this.passwordErrors > 0 || this.email.length == 0 || this.password.length == 0) {
+        this.$v.$touch()
+        this.isLoading = false
+      } else {
+        this.isLoading = false
+        let data = {
+          email: this.email,
+          password: this.password,
+        }
+        
+        this.login(data)
+        this.$router.push('/')
+
+       /*  if (this.isAuthenticated) {
+          this.isLoading = false
+          // this.$router.push('/')
+        } else if (!this.isAuthenticated) {
+          // this.isLoading = false
+          // this.errorMsg = 'Your email or password is incorrect.'
+        } */
       }
-      this.login(data)
     },
+    ...mapGetters({
+      isAuthenticated: 'isAuthenticated',
+      errors: 'getErrors',
+    }),
     ...mapActions({
       login: 'CustomerStore/login',
     }),
   },
   computed: {
-    
-  }
+    emailErrors () {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be a valid email')
+      !this.$v.email.required && errors.push('Email is required.')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.required && errors.push('Password is required.')
+      return errors
+    },
+  },
 }
 </script>
