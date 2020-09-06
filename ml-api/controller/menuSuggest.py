e2cb2fddf4e13986986694eb13e77d8b79d2465b
Giovanni Joubert <u18009035@tuks.co.za>
2020-09-06 15:01:59 +0200

    read, format and group ratingData

--- ml-api/controller/menuSuggest.py
+++ ml-api/controller/menuSuggest.py
@@ -3,4 +3,5 @@ import controller.ratingEstimator as re
 from flask import jsonify
 
 def suggest():
+    re.filterRatingData()
     return jsonify(re.retrieveRatingData())
--- ml-api/controller/ratingEstimator.py
+++ ml-api/controller/ratingEstimator.py
@@ -1,5 +1,6 @@
 # ratingEstimator Module - uses collaborative filtering to estimate a user's rating on a menu item
 import sys
+import json
 sys.path.append('..') #import from parent directory
 
 import db
@@ -15,3 +16,22 @@ def retrieveRatingData():
     cursor.close()
     db.close(connection)
     return records
+
+def filterRatingData():
+    import pandas as pd
+    from math import sqrt
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    #read and filter the data
+    ratings_df = pd.DataFrame.from_records(retrieveRatingData())
+    ratings_df.columns = ['customerId', 'menuItemId', 'ratingScore']
+    ratings_df = ratings_df.dropna()
+    ratings_df['menuItemId'] = ratings_df['menuItemId'].astype(int)
+
+    #Groupby creates several sub dataframes where they all have the same value in the column specified as the parameter
+    ratingsGrouped = ratings_df.groupby('customerId')
+    #Sorting it so users with menuItems most in common with the input will have priority
+    ratingsGrouped = sorted(ratingsGrouped, key=lambda x: len(x[1]), reverse=True)
+
+    print(ratingsGrouped)
