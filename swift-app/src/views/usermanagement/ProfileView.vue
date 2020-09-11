cd5198d5c3731fa1dfc7e8ba4588dc16ee09aec0
Cadon <cadongernandt@gmail.com>
2020-09-11 16:20:02 +0200

    Upload profile picture

--- swift-app/src/views/usermanagement/ProfileView.vue
+++ swift-app/src/views/usermanagement/ProfileView.vue
@@ -6,17 +6,36 @@
       </v-col>
       <v-col cols="2">
         <v-btn width="30px" height="30px" @click="editProfile" color="secondary" absolute small fab style="top: 20px; right: 15px;">
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
       </v-col>
     </v-row>
     <v-row class="mt-0 pt-0" align="center">
+      <v-col cols="12" class="pt-0" align="center">
+        <image-input v-model="avatar">
+          <div slot="activator">
+            <v-avatar height="120" width="120" v-ripple v-if="!avatar"  class="grey lighten-3 mb-3">
+              <v-img :src="customerInfo.profileimageurl" cover alt="avatar"></v-img>
+            </v-avatar>
+            <v-avatar height="120" width="120" v-ripple v-else class="mb-3">
+              <v-img :src="avatar.imageURL" cover alt="avatar"></v-img>
+            </v-avatar>
+          </div>
+        </image-input>
+        <!-- <v-slide-x-transition>
+          <v-btn v-if="avatar && saved == false" width="30px" height="30px" @click="editProfile" color="secondary" absolute small fab style="top: 90px; right: 115px;">
+            <v-icon @click="uploadImage" :loading="saving">mdi-check</v-icon>
+          </v-btn>
+        </v-slide-x-transition> -->
+      </v-col>
+    </v-row>
+    <!-- <v-row class="mt-0 pt-0" align="center">
       <v-col cols="12" class="pt-0" align="center">
         <v-avatar height="120" width="120">
           <img :src="customerInfo.profileimageurl" alt="John"/>
         </v-avatar>
       </v-col>
-    </v-row>
+    </v-row> -->
     <v-row>
       <v-col cols="12" class="py-0" align="center">
         <span style="font-size: 20px">{{customerInfo.name}} {{customerInfo.surname}}</span>
@@ -100,9 +119,21 @@
 
       </v-tab-item>
       <v-tab-item>
-        <v-card flat>
-          <v-card-text>No settings yet</v-card-text>
-        </v-card>
+        <v-list subheader>
+          <v-list-item @click=removeAccount()  v-ripple>
+            <v-list-item-avatar class="grey lighten-2 secondary--text">
+              <v-icon>mdi-account-remove</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title >Remove Account</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon color="secondary">mdi-chevron-right</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
       </v-tab-item>
     </v-tabs-items>
     <v-btn v-if="checkedIn()" @click="goToCart" fixed app color="primary" width="52px" height="52px" elevation="1" absolute dark bottom style="right: 50%; transform: translateX(50%); bottom: 30px; z-index: 100;" fab>
@@ -117,6 +148,7 @@
 import NavBar from '@/components/layout/NavBar';
 import store from '@/store/store.js';
 import { mapActions, mapGetters, mapMutations } from 'vuex'
+import ImageInput from '../../components/imageUploader/imageInput.vue'
 
 export default {
   data: () => ({
@@ -128,10 +160,22 @@ export default {
       { icon: 'mdi-logout', iconClass: 'grey lighten-2 secondary--text', title: 'Logout', route: '', },
     ],
     tab: null,
-    darkMode: null
+    darkMode: null,
+    avatar: null,
+    saving: false,
+    saved: false
   }),
   components: {
-    'NavBar': NavBar
+    'NavBar': NavBar,
+    ImageInput: ImageInput
+  },
+  watch:{
+    avatar: {
+      handler: function() {
+        this.saved = false
+      },
+      deep: true
+    }
   },
   methods: {
     signOut () {
@@ -144,9 +188,17 @@ export default {
     editProfile () {
     },
     changeTheme() {
-      console.log(this.darkMode)
       this.darkMode = !this.darkMode;
-      // call update theme
+      // await update theme
+    },
+    uploadImage() {
+      this.saving = true
+      setTimeout(() => this.savedAvatar(), 1000)
+    },
+    savedAvatar() {
+      // await save image
+      this.saving = false
+      this.saved = true
     },
     mounted() {
       if (this.customerInfo.theme == 'light') {
