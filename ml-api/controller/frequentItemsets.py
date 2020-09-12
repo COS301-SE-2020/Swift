671e9b57b55f6545ce2369fd371504f6909b8124
Giovanni Joubert <u18009035@tuks.co.za>
2020-09-12 22:58:37 +0200

    implement Apriori algorithm & return grouped items

--- ml-api/controller/frequentItemsets.py
+++ ml-api/controller/frequentItemsets.py
@@ -9,7 +9,7 @@ import db
 def retrieveOrderData():
     connection = db.connect()
     cursor = connection.cursor()
-    cursor.execute("SELECT customerid,  menuitemid, quantity FROM itemordered" +
+    cursor.execute("SELECT customerorder.orderid, customerid,  menuitemid, quantity FROM itemordered" +
                     " INNER JOIN customerorder" +
                     " ON customerorder.orderid = itemordered.orderid;")
     records = cursor.fetchall()
@@ -27,4 +27,50 @@ def clearOrdersCache():
     return {'message': 'Cache Cleared'}
 
 def Apriori(viz = False):
-    return retrieveOrderData()
+    import pandas as pd
+    from mlxtend.frequent_patterns import apriori
+    from mlxtend.frequent_patterns import association_rules
+    from mlxtend.preprocessing import TransactionEncoder
+
+    #read and filter the data
+    orders_df =  pd.DataFrame.from_records(retrieveOrderData())
+    orders_df.columns = ['orderId','customerId', 'menuItemId', 'quantity']
+    orders_df = orders_df.dropna()
+
+    #convert to a basket of orderId (row) x menuItemId (col) (which items are present in which orders)
+    orderBasket = (orders_df.groupby(['orderId', 'menuItemId'])['quantity']
+            .sum().unstack().reset_index().fillna(0)
+            .set_index('orderId'))
+    
+    #convert all values >=1 to 1 and all <=0 to 0
+    def encodeData(x):
+        if x <= 0:
+            return 0
+        if x >= 1:
+            return 1
+    basket_sets = orderBasket.applymap(encodeData)
+    
+    #apply mlxtend apriori to the basket, items need to occur together in an order 
+    #for at least 1.5% of all orders in database
+    frequent_itemsets = apriori(basket_sets, min_support=0.015, use_colnames=True, low_memory=True)
+    frequent_itemsets.sort_values('support', ascending=False)
+
+    associationRules = association_rules(frequent_itemsets, metric="lift", min_threshold=0)
+
+    #sort association by lift then confidence, return top 20
+    associationRules = (associationRules.sort_values(['lift', 'confidence'], ascending=False))[0:20]
+
+    #group all of the top associated items
+    groupedItems = []
+
+    for indx in associationRules.index: 
+        group = []
+        for antecedent in associationRules["antecedents"][indx]:
+            group.append(antecedent)
+            
+        for consequent in associationRules["consequents"][indx]:
+            group.append(consequent)
+
+        groupedItems.append(group)
+    
+    return groupedItems
